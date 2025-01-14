b7adc1930455a9bd784269d97b8fe9f084cb15eb
PARIMAL BHAWANE <parimalbhawane@gmail.com>
2025-01-14 20:01:44 +0530

    add variable declaration examples and notes in JavaScript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,72 @@
+const accountId = 144553
+let accountEmail = "parimalbhawane@google.com"
+var accountPassword = "12345"
+accountCity = "Akola"
+let accountState; // undifined
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "pari@bhawane.com"
+accountPassword = "21212121"
+accountCity = "Pune"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+// how to create table in javascript
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+//***************************************************************************** Notes *****************************************************************************
+          /*variables*/
+// Three ways to declare variables in JS
+// -> const
+// -> let
+// -> var
+
+         /*Rulse for declaring variables*/
+// -> variable name should start with letter, $, _
+// -> variable name should not start with number
+// -> variable name should not be reserved keyword
+// -> variable name should be camelCase
+// -> variable name should be meaningful
+// -> variable name should be short
+// -> variable name should be readable
+// -> variable name should be understandable
+// -> variable name should be in english
+// -> variable name should be in lower case
+// -> variable name should be in upper case
+// -> variable name should be in title case
+// -> variable name should be in pascal case
+// -> variable name should be in snake case
+// -> variable name should be in kebab case
+// -> variable name should be in dot case
+// -> variable name should be in constant case
+// -> variable name should be in camel case
+// -> variable name should be in hungarian notation
+// -> variable name should be in system case
+
+         /*What is JavaScript*/
+// -> semi-colon is optional in JS
+// -> JS is case sensitive
+// -> JS is loosely typed language
+// -> JS is dynamically typed language
+// -> JS is interpreted language
+// -> JS is single threaded language
+// -> JS is synchronous language
+// -> JS is non-blocking language
+// -> JS is event driven language
+// -> JS is functional language
+// -> JS is object oriented language
+// -> JS is prototype based language
+// -> JS is weakly typed language
+// -> JS is garbage collected language
+// -> JS is platform independent language
+// -> JS is open source language
+// -> JS is client side language
+// -> JS is server side language
+// -> JS is full stack language
