0e501440362d024fb7298bacbd18326a2aba38ea
PARIMAL BHAWANE <parimalbhawane@gmail.com>
2025-01-15 01:14:38 +0530

    Comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,40 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0); 
+console.log(null >= 0); // Comparions(>=) with null and zero always return true
+console.log(null <= 0); // Comparions(<=) with null and zero always return true
+
+/*console.log(undefined == 0);
+console.log(undefined > 0);  // Comparions with undifined  always return false
+console.log(undefined < 0);
+*/
+
+// === 
+
+console.log("2" === 2);
+
+// ********************************************************************* Notes *********************************************************************
+
+// 1. == (Equality) => It will compare the values of the operands and return true if they are equal.
+// 2. === (Identity) => It will compare the values and the type of the operands and return true if they are equal.
+// 3. != (Inequality) => It will compare the values of the operands and return true if they are not equal.
+// 4. !== (Non-identity) => It will compare the values and the type of the operands and return true if they are not equal.
+// 5. > (Greater than) => It will return true if the left operand is greater than the right operand.
+// 6. < (Less than) => It will return true if the left operand is less than the right operand.
+// 7. >= (Greater than or equal to) => It will return true if the left operand is greater than or equal to the right operand.
+// 8. <= (Less than or equal to) => It will return true if the left operand is less than or equal to the right operand.
+
+
+
+ // ** links ** //
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
+    // https://tc39.es/ecma262/#sec-ecmascript-data-types-and-values
