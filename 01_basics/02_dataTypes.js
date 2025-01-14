e5699823f12888f9facba855a1ff50e180236a51
PARIMAL BHAWANE <parimalbhawane@gmail.com>
2025-01-14 23:15:17 +0530

    Data Types in JavaScript

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,58 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Parimal")
+
+
+let name = "parimal"
+let age = 23
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+//***************************************************************************** Notes *****************************************************************************
+
+//          /*Data Types*/
+// -> number
+/* 2 to power 53 */
+
+// -> bigint
+/* 2n */ /* Numeric Primitive in js that can represent integers with arbitary magnitude.*/
+
+// -> string
+/* "" */ /* Represent  textual dara and is encoded as a sequence of 16-bit unsigned integer values representing UTS-16 code units.*/
+
+// -> boolean
+/* true/false */ /* Represents a logical entity and can have two values: true or false.*/
+
+// -> null
+/* null */ /* Represents the intentional absence of any object value.*/
+
+// -> undefined
+/* undefined */ /* Represents the value of uninitialized variables.*/
+
+// -> symbol
+/* unique */ /* Represents a unique identifier.Immutable primitive value and may be used as the key of an objects Symbols and called "atoms".*/
+
+// -> object
+/* {} */ /* Represents a collection of key-value pairs.*/
+/*  const obj = {
+    name: "John",
+    age: 30
+} */
